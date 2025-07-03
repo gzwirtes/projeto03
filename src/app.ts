f86ddc1d5c4972c7379a7ab6bc445b31d2672f9c
Gustavo Zwirtes <gustavo.zts@gmail.com>
2025-07-03 17:13:51 -0300

    tratamento de erros funcionando

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,27 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/controllers/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.format(),
+    })
+  }
+
+  if (env.NODE_ENV !== 'prod') {
+    console.error(error)
+  } else {
+    // TODO: fazer algum erro de log para uma ferramento que avisa sobre o erro DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({
+    message: 'Internal server error',
+  })
+})
